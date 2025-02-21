24cfaf1f4b7aeacae68912ac5250981666834370
Otger Comas Valls <otgercomas@gmail.com>
2025-02-21 19:00:47 +0100

    Better l command

--- config/rc.bash
+++ config/rc.bash
@@ -20,18 +20,19 @@ esac
 ######################
 
 if [ "$(tput colors)" -ge 8 ]; then
-  reset=$'\e[0m'
-  black=$'\e[1;30m'
-  red=$'\e[1;31m'
-  green=$'\e[1;32m'
-  yellow=$'\e[1;33m'
-  blue=$'\e[1;34m'
-  purple=$'\e[1;35m'
-  cyan=$'\e[1;36m'
-  white=$'\e[1;37m'
+  export reset=$'\e[0m'
+  export black=$'\e[1;30m'
+  export red=$'\e[1;31m'
+  export green=$'\e[1;32m'
+  export yellow=$'\e[1;33m'
+  export blue=$'\e[1;34m'
+  export purple=$'\e[1;35m'
+  export cyan=$'\e[1;36m'
+  export white=$'\e[1;37m'
 
   alias ls='ls --color=auto'
 fi
+unalias l
 
 #####################
 ####### Alias #######
