93bac5b5bb3abd4bc2268ef4e7eddb685803816a
otcova <otgercomas@gmail.com>
2025-10-09 21:25:33 +0200

    kanata

---
+++ config/nvim/KEY_MAPS.md
@@ -0,0 +1,29 @@
+# Lsp
+
+<ctrl>k - hover diagnostic
+<leader>d - list diagnostics
+
+
+# Text Objects
+
+## mini.ai
+vaf - around function
+vaa - around argument
+vab - around brackets
+vaq - around quotes
+
+
+# Pickers
+
+## Telescope
+
+<leader>f   - files (- git ignored)
+<leader>pf  - files (+ git ignored)
+<leader>paf - all files
+
+<leader>pg  - grep files
+<leader>pag - grep all files
+
+
+
+
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,11 +3,22 @@ require("init_lazy")
 -- Common keymaps
 vim.keymap.set("n", "<c-k>", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>q", "<cmd>silent qa<CR>")
-vim.keymap.set({"n", "v"}, "<leader>j", "<esc>40jzt")
-vim.keymap.set({"n", "v"}, "<leader>k", "<esc>40kzt")
 vim.keymap.set({"n", "v"}, "<leader>a", "<cmd>e#<cr>")
 
 vim.opt.scrolloff = 3
+
+-- Slide Scroll
+vim.keymap.set({"n", "v"}, "<leader>j", function()
+    local line = vim.fn.line(".")
+    local page_line = math.floor(line / 100 + 1) * 100
+    vim.cmd("normal! " .. math.max(1, page_line - line) .. "jzt")
+end)
+vim.keymap.set({"n", "v"}, "<leader>k", function()
+    local line = vim.fn.line(".")
+    local page_line = math.floor(line / 100 - 1) * 100
+    vim.cmd("normal! " .. math.max(1, line - page_line) .. "kzt")
+end)
+
 
 
 
